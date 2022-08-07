e237cbfd2142092ff51a02a6366f108bb0b3fddf
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-08-08 00:07:59 +0530

    Deploy

--- server.js
+++ server.js
@@ -15,9 +15,6 @@ const app = express();
 
 app.use(express.json());
 
-// if(process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
 
 app.use('/api/v1/transactions', transactions);
 
